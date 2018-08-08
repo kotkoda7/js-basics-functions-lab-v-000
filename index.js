1438b73c963593c115bfbdcaa16a6328cbd7a531
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-08-08 13:49:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,5 +29,7 @@ function calculatesFarePrice(start, destination) {
   else if (distance > 2000 && distance < 2500) {
     return 25;
   }
-  else 
+  else {
+    return 'cannot travel that far';
+  }
 }
