aa611395d34c3f348e5520c145a49f349c2fd622
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-08-08 13:29:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks (blockNumber) {
   if (blocknumber > 42) {
-    return 
+    return blocknumber - 42
+  } else {
+    return 42 - blocknumber
   }
 }
