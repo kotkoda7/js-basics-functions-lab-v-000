1b0883376671647d4904360c9468d258f5e73cf3
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-08-08 13:47:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ function calculatesFarePrice(start, destination) {
   const distance = distanceTravelledInFeet(start, destination);
   if (distance <= 400) {
     return 0; }
-  else if (distance > 400 && distance < 2000) {
-    return 
+  else if (distance > 400 && distance <= 2000) {
+    return 0.2 * ()
   }
   
 }
